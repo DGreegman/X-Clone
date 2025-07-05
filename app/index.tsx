5762c290113605a6d2a668b02eb4817f1dea84c6
DGreegman <obeagug@gmail.com>
2025-07-05 15:33:34 +0100

    Initial setup with Nativewind

---
+++ app/index.tsx
@@ -0,0 +1,11 @@
+import { Text, View } from "react-native";
+
+export default function Index() {
+  return (
+    <View
+      className="flex-1 justify-center items-center bg-blue-700"
+    >
+      <Text className="text-red-400">Edit app/index.tsx to edit this screen. Great</Text>
+    </View>
+  );
+}
