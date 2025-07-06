c89ee4aa832ba0c8b85a271f179016c1deda4ac1
DGreegman <obeagug@gmail.com>
2025-07-06 20:34:13 +0100

    Added Message Screen Feature

--- app/_layout.tsx
+++ app/_layout.tsx
@@ -8,7 +8,7 @@ export default function RootLayout() {
     <ClerkProvider tokenCache={tokenCache}>
       <Stack screenOptions={{headerShown: false}}>  
         <Stack.Screen name="(auth)" options={{headerShown: false}}/>
-        <Stack.Screen name="index" options={{headerShown: false}}/>
+        
       </Stack>
     </ClerkProvider>
   );
